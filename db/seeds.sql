ef2a2f4ad4c8674796526b39797806373afecb47
gbluemd <gsjujub@gmail.com>
2020-09-24 18:31:59 -0600

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+USE burgers_db;
+
+
+INSERT INTO burgers (burger_name) VALUES ("Tang Hamberder");
+INSERT INTO burgers (burger_name) VALUES ("SnowBall Hamberder");
+INSERT INTO burgers (burger_name) VALUES ("Shroom Hamberder");
+INSERT INTO burgers (burger_name) VALUES ("Foghorn Hamberder");
